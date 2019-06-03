b07716817e61325c93514a83a128b329be06a073
sputnika <sputnika91@gmail.com>
2019-06-03 10:39:20 -0400

    first run-part 2

--- src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
@@ -11,7 +11,8 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return an array of strings, each element representative of a respective word in the sentence
      */
     public static String[] getWords(String sentence) {
-        return null;
+        String [] answer = sentence.split(" ");
+        return answer;
     }
 
 
@@ -21,7 +22,9 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word of the sentence
      */
     public static String getFirstWord(String sentence) {
-        return null;
+        int x = sentence.indexOf(' ');
+        String firstWord = sentence.substring(0,x);
+        return firstWord;
     }
 
     /**
@@ -30,7 +33,13 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order
      */
     public static String reverseFirstWord(String sentence) {
-        return null;
+        int x = sentence.indexOf(' ');
+        String firstWord = sentence.substring(0, x);
+        String rev = "";
+        for (int y = firstWord.length() - 1; y >= 0; y--) {
+            rev = rev + firstWord.charAt(y);
+        }
+        return rev;
     }
 
     /**
@@ -39,7 +48,16 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order with the first character capitalized
      */
     public static String reverseFirstWordThenCamelCase(String sentence) {
-        return null;
+        int x = sentence.indexOf(' ');
+        String firstWord = sentence.substring(0, x);
+        String rev = "";
+        for (int y = firstWord.length() - 1; y >= 0; y--) {
+            rev = rev + firstWord.charAt(y);
+        }
+        rev = rev.substring(0,1).toUpperCase()+rev.substring(1);
+
+        return rev;
+
     }
 
 
@@ -50,7 +68,8 @@ public class StringUtils {
      * given a string and index, return an identical string excluding the character at the specified index
      */
     public static String removeCharacterAtIndex(String str, int index) {
-        return null;
+        str = str.substring(0,index) + str.substring(index+1);
+        return str;
     }
 
 }
