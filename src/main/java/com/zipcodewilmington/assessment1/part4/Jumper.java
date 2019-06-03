b0c40a4d8aa1678d97e6c5091364e40086d70e7c
sputnika <sputnika91@gmail.com>
2019-06-03 16:36:24 -0400

    update

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,9 +6,34 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
+        int count =0;
+        int jumpheight =0;
+        for (int x=0; x<=k; x++){
+            if(x != k){
+                count++;
+            }
+
+        }
+        while(j<=k){
+            j+=j;
+            jumpheight++;
+        }
+
+
+        return jumpheight;
+
+
+        /*
         if(k == j)
             return (1);
 
+
         return -1;
+    */
+
+
     }
+
+
+
 }
